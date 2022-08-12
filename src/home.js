6dd981300da38abce54bccd6aa0c40f76149f8ee
Rene <renesuar99@gmail.com>
2022-08-11 21:39:20 -0400

    created home tab

--- src/home.js
+++ src/home.js
@@ -1,13 +1,18 @@
 
 
 function homeCreate() {
-const container = document.getElementById('content')
+ const container = document.getElementById('content')
+    if (container.lastChild){
+    container.removeChild(container.lastChild);
+}
+
 const header = document.createElement('h1');
 header.innerText = "Rene's Italian Restaurant"
 header.style.color = "white";
 container.style.backgroundColor = "black";
 container.appendChild(header);
+
 }; 
 
 
-export {homeCreate};
+export {homeCreate}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import './style.css'; 
 import {homeCreate} from './home.js';
 import {removeFromContainer} from './menu';
+const homeButton = document.getElementById('home');
+const header = document.createElement('h1');
+const menuButton = document.getElementById('menu');
 homeCreate();
 removeFromContainer();
+
+
+homeButton.addEventListener('click', function(e) {
+    const header = document.createElement('h1');
+    homeCreate();
+   
+    
+});
+
+menuButton.addEventListener('click', function(e) {
+  
+    removeFromContainer();
+    
+});
